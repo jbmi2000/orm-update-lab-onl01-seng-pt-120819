adcbab9e118b630265312de55cfdc6d48f45b312
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-03-03 02:21:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,7 +51,7 @@ class Student
     end
 
     def self.create(name:, grade:)
-      new_student = Student.new(name, grade)
+      new_student = Student.new(@name, @grade)
       new_student.id = row[0]
       neW_student.name = row[1]
       new_student.grade = row[2]
